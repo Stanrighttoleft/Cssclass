1eda140746614da11eab4750099f5b6f8f02727f
stan <moon_saga@yahoo.com.tw>
2025-08-14 17:49:46 +0800

    try

--- projectweb-vue/src/components/Footer.vue
+++ projectweb-vue/src/components/Footer.vue
@@ -1,84 +1,92 @@
 <template>
-    <motion.div :class="[{active:startAnim },'footerdiv position-relative  container-fluid bg-secondary text-white pt-3']"
-    :initial="{opacity:0}"
-    :whileInView="{opacity:1}"
-    @viewport-enter="startAnim=true"
-    @viewport-leave="startAnim=false"
-
-    >
-        <div class="row">
-            <div class="col-md-3">掃我到客服</div>
-            <div class="col-md-3"><img class="img-fluid" src="/assets/logov12.png" alt=""></div>
-            <div class="col-md-6 d-flex justify-content-center">
-                <ul class="list-unstyled">
-                    <li class="">澳丁團購</li>
-                    <li class="">會員登入</li>
-                    <li class="">購物流程</li>
-                    <li class="">隱私權聲明</li>
-                    <li class="">會員條款</li>
-                    <li class="">FAQ問與答</li>
-                    <li class="">聯絡我們</li>
-                    <li class=" ">追蹤澳丁</li>
-
-                </ul>
-            </div>
-        </div>
-        <hr>
-        <div class="d-flex justify-content-center">copyright</div>
-    </motion.div>
-    
-  
+  <motion.div
+    :class="[
+      { active: startAnim },
+      'footerdiv position-relative  container-fluid bg-secondary text-white pt-3',
+    ]"
+    :initial="{ opacity: 0 }"
+    :whileInView="{ opacity: 1 }"
+    @viewport-enter="startAnim = true"
+    @viewport-leave="startAnim = false"
+  >
+    <div class="row">
+      <div class="col-md-3">掃我到客服</div>
+      <div class="col-md-3">
+        <img class="img-fluid" src="/assets/logov12.png" alt="" />
+      </div>
+      <div class="col-md-6 d-flex justify-content-center">
+        <ul class="list-unstyled">
+          <li class="">澳丁團購</li>
+          <li class="">會員登入</li>
+          <li class="">購物流程</li>
+          <li class="">隱私權聲明</li>
+          <li class="">會員條款</li>
+          <li class="">FAQ問與答</li>
+          <li class="">聯絡我們</li>
+          <li class=" ">追蹤澳丁</li>
+        </ul>
+      </div>
+    </div>
+    <hr />
+    <div class="d-flex justify-content-center">copyright</div>
+  </motion.div>
 </template>
 
 <script setup>
-import { ref } from 'vue';
-import { motion } from 'motion-v';
+import { ref } from "vue";
+import { motion } from "motion-v";
 
-const startAnim=ref(false)
-
+const startAnim = ref(false);
 </script>
 
 <style>
-.footerdiv::before{
-    content: '';
-    position:absolute;
-    display:block;
-    background-image: url('/assets/footer.png');
-    background-size: auto 100%;
-    gap: 0;
-    width:200%;
-    height: 200px;
-    background-repeat: repeat-x;
-   
-    margin: 0;
-    padding: 0;
-   
-    bottom: 100%;
-    left:0;
-    z-index: 0;
-    
-}
-.footerdiv.active::before{
-    animation:pulse 2s ease-in-out;
-}
+.footerdiv::before,
+.footerdiv::after {
+  content: "";
+  position: absolute;
+  display: block;
+  background-image: url("/assets/footer.png");
+  background-size: auto 100%;
+  gap: 0;
+  width: 100%;
+  height: 200px;
+  background-repeat: repeat-x;
 
-@keyframes pulse{
-    0%{
-        transform:translateX(0) translateY(0) scale(1);
-        
-        
+  margin: 0;
+  padding: 0;
 
-    }
-    50%{
-        transform:translateX(-20%) translateY(1%) scale(1.1);
-        
-        
-    }
-    100%{
-        transform:translateX(0) translateY(0) scale(1);
-        
-        
-    }
+  bottom: 100%;
+  left: 0;
+  z-index: 0;
+}
+.footerdiv.active::before {
+  animation: pulse1 8s ease;
+}
+.footerdiv.active::after {
+  animation: pulse2 8s ease-in-out;
 }
 
+@keyframes pulse1 {
+  0% {
+    background-position-x: 0px;
+  }
+
+  50% {
+    background-position-x: -500px;
+  }
+  100% {
+    background-position-x: 0px;
+  }
+}
+@keyframes pulse2 {
+  0% {
+    background-position-x: -300px; /* half of your PNG's width */
+  }
+  50% {
+    background-position-x: -900px; /* half offset + one full width */
+  }
+  100% {
+    background-position-x: 0px; /* half offset + one full width */
+  }
+}
 </style>
--- projectweb-vue/src/components/openBanner.vue
+++ projectweb-vue/src/components/openBanner.vue
@@ -1,137 +1,143 @@
 <script setup>
+import { ref } from "vue";
+const rabbit = ref("/assets/rabbit.png");
+const fu = ref("/assets/fun.png");
+const cloud = ref("/assets/cloud.svg");
+const cloud2 = "/assets/cloud2.svg";
+import { motion, useScroll } from "motion-v";
 
-import { ref } from 'vue';
-const rabbit = ref('/assets/rabbit.png');
-const fu = ref('/assets/fun.png')
-const cloud = ref('/assets/cloud.svg')
-const cloud2 = '/assets/cloud2.svg'
-import { motion, useScroll } from 'motion-v';
-
-const { scrollProgress } = useScroll()
+const { scrollProgress } = useScroll();
 
 const scrollIndicator = {
-    rotate: scrollProgress,
-}
-
-
-
-
-
+  rotate: scrollProgress,
+};
 </script>
 
 <template>
-    <div class="open d-flex justify-content-center align-items-center position-relative">
-
-        <motion.div :initial="{
-            x: 0,
-            opacity: 0,
-            scale: 0.8
-        }" :whileInView="{
-            y: [0, -50, 0],
-            opacity: 1,
-            transition: { duration: 2 },
-            scale: 1
-        }" :whilePress="{ y: -50 }">
-            <img :src=rabbit alt="">
-        </motion.div>
-        <div class="fu">
-            <motion.img :src=fu alt="" :initial="{ rotate: 0 }" :whileInView="{
-                rotate: 180,
-                transition: {
-                    delay: 0.5,
-                    duration: 1
-                }
-            }"></motion.img>
-        </div>
-        <div class="cloud position-absolute ">
-            <motion.img :src=cloud alt="" :initial="{
-                x: 0,
-                opacity: 0
-            }" :whileInView="{
-                    x: 100,
-                    opacity: 0.7,
-                    transition: {
-                        delay: 0.5,
-                        duration: 1,
-                    }
-                }"></motion.img>
-        </div>
-        <div class="cloud2 position-absolute ">
-            <motion.img :src=cloud2 alt="" :initial="{
-                x: 0,
-                opacity: 0
-            }" :whileInView="{
-                    x: -100,
-                    opacity: 0.7,
-                    transition: {
-                        delay: 0.5,
-                        duration: 1,
-                    }
-                }"></motion.img>
-        </div>
-
+  <div
+    class="open d-flex justify-content-center align-items-center position-relative"
+  >
+    <motion.div
+      :initial="{
+        x: 0,
+        opacity: 0,
+        scale: 0.8,
+      }"
+      :whileInView="{
+        y: [0, -50, 0],
+        opacity: 1,
+        transition: { duration: 2 },
+        scale: 1,
+      }"
+      :whilePress="{ y: -50 }"
+    >
+      <img :src="rabbit" alt="" />
+    </motion.div>
+    <div class="fu">
+      <motion.img
+        :src="fu"
+        alt=""
+        :initial="{ rotate: 0 }"
+        :whileInView="{
+          rotate: 180,
+          transition: {
+            delay: 0.5,
+            duration: 1,
+          },
+        }"
+      ></motion.img>
     </div>
-
+    <div class="cloud position-absolute">
+      <motion.img
+        :src="cloud"
+        alt=""
+        :initial="{
+          x: 0,
+          opacity: 0,
+        }"
+        :whileInView="{
+          x: 100,
+          opacity: 0.7,
+          transition: {
+            delay: 0.5,
+            duration: 1,
+          },
+        }"
+      ></motion.img>
+    </div>
+    <div class="cloud2 position-absolute">
+      <motion.img
+        :src="cloud2"
+        alt=""
+        :initial="{
+          x: 0,
+          opacity: 0,
+        }"
+        :whileInView="{
+          x: -100,
+          opacity: 0.7,
+          transition: {
+            delay: 0.5,
+            duration: 1,
+          },
+        }"
+      ></motion.img>
+    </div>
+  </div>
 </template>
 
 <style>
 .open {
-    background-image: url('/assets/bg-banner.jpg');
-    background-size: cover;
-    height: 670px;
-    overflow: hidden;
-
-
-
-
+  background-image: url("/assets/bg-banner.jpg");
+  background-size: cover;
+  height: 670px;
+  overflow: hidden;
 }
 
 .open::before {
-    content: '';
-    position: absolute;
-    display: block;
-    width: 100%;
-    height: 300px;
+  content: "";
+  position: absolute;
+  display: block;
+  width: 100%;
+  height: 300px;
 
-    bottom: 0%;
-    background-image: url('/assets/grass.svg');
-    background-size: contain;
-    background-repeat: repeat-x;
-    background-position: center;
-    z-index: 2;
+  bottom: 0%;
+  background-image: url("/assets/grass.svg");
+  background-size: contain;
+  background-repeat: repeat-x;
+  background-position: center;
+  z-index: 2;
 }
 
 .open::after {
-    content: '';
-    position: absolute;
-    top: 0;
-    left: 0;
-    right: 0;
-    bottom: 0;
-    background-color: rgba(255, 255, 255, 0.3);
-    z-index: 1;
-    pointer-events: none;
+  content: "";
+  position: absolute;
+  top: 0;
+  left: 0;
+  right: 0;
+  bottom: 0;
+  background-color: rgba(255, 255, 255, 0.3);
+  z-index: 1;
+  pointer-events: none;
 }
 
 .fu {
-    transform: translate(-40%, -80%)
+  transform: translate(-40%, -80%);
 }
 
 .cloud {
-    width: 500px;
-    height: 500px;
-    top: 50%;
-    left: 0;
-    z-index: 3;
-
+  width: 500px;
+  height: 500px;
+  top: 50%;
+  left: 0;
+  z-index: 3;
 }
 
 .cloud2 {
-    width: 600px;
-    height: 600px;
-    top: 60%;
-    right: 0;
-    z-index: 3;
-
+  width: 600px;
+  height: 600px;
+  top: 60%;
+  right: 0;
+  z-index: 3;
 }
 </style>
