51235941805c785dba575e74fd4c858338b14a01
stan <moon_saga@yahoo.com.tw>
2025-08-26 19:40:21 +0800

    dont know waht change

--- projectweb-vue/src/views/CartListView.vue
+++ projectweb-vue/src/views/CartListView.vue
@@ -14,20 +14,3 @@ import CartList from "@/components/CartList.vue";
   height: 100vh;
 }
 </style>
-
-<template>
-  <div class="conainer-fluid cartlist">
-    <CartList />
-  </div>
-</template>
-
-<script setup>
-import CartList from "@/components/CartList.vue";
-</script>
-
-<style>
-.cartlist {
-  margin-top: 100px;
-  height: 100vh;
-}
-</style>
