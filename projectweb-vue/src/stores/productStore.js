3ecbbe43ef2f24a641f0470dc2bc050df7c86a7a
stan <moon_saga@yahoo.com.tw>
2025-08-31 21:04:24 +0800

    products work

--- projectweb-vue/src/stores/productStore.js
+++ projectweb-vue/src/stores/productStore.js
@@ -7,9 +7,11 @@ export const useProductStore = defineStore("product", () => {
   const product = ref([]);
   const fetchProducts = async () => {
     try {
-      const response = await request.get("api/products");
+      // const response = await request.get("api/products");
+      const response = await request.get("/products.php");
 
-      products.value = response.data.data.nutrition;
+      // products.value = response.data.data.nutrition;
+      products.value = response.data.data;
 
       console.log("Products assigned to store:", products.value);
     } catch (error) {
@@ -18,7 +20,8 @@ export const useProductStore = defineStore("product", () => {
   };
   const fetchProduct = async (id) => {
     try {
-      const response = await request.get(`api/products/${id}`);
+      // const response = await request.get(`api/products/${id}`);
+      const response = await request.get(`/product.php?id=${id}`);
       product.value = response.data.data;
       console.log("product assigned to store:", product.value);
     } catch (error) {
