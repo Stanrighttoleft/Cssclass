7e138ae79564396163a617765bb1d5fd5d0bac89
stan <moon_saga@yahoo.com.tw>
2025-11-11 19:10:39 +0800

    practice nexttick

---
+++ projectweb-vue/src/components/temp.vue
@@ -0,0 +1,26 @@
+<template>
+  <div>
+    <button @click="list.push(list.length + 1)">追加</button>
+    <ul ref="ullist">
+      <li v-for="(item, index) in list" :key="index">{{ item }}</li>
+    </ul>
+  </div>
+</template>
+
+<script setup>
+import { ref, watch, nextTick } from "vue";
+
+const list = ref([]);
+const ullist = ref(null);
+watch(
+  list,
+  async () => {
+    console.log("一般", ullist.value?.offsetHeight);
+    await nextTick();
+    console.log("nextTick", ullist.value?.offsetHeight);
+  },
+  { deep: true }
+);
+</script>
+
+<style></style>
--- projectweb-vue/src/views/FormView.vue
+++ projectweb-vue/src/views/FormView.vue
@@ -1,11 +1,14 @@
 <template>
   <div class="container-fluid" style="height: 100vh; margin-top: 150px">
     <SelectionForm />
+    <hr />
+    <temp />
   </div>
 </template>
 
 <script setup>
 import SelectionForm from "@/components/SelectionForm.vue";
+import temp from "@/components/temp.vue";
 </script>
 
 <style></style>
